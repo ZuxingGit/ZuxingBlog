381d1d83117b3dd692cdbfdcd6001d83f56cb1d4
Zuxing_a1816653 <a1816653@adelaide.edu.au>
2025-12-17 16:36:27 +1030

    Tab label changed

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
 export const SITE = 'https://jsun.lol';
 export const HEADER_IMAGE = SITE + '/header.png';
-export const SITE_NAME = 'Jsun Dot LOL';
-export const TWITTER_ACCOUNT = '@jsun969';
-export const DESCRIPTION = "LOL! Welcome to Justin Sun's personal website!";
+export const SITE_NAME = 'Zuxing Blog';
+export const TWITTER_ACCOUNT = '';
+export const DESCRIPTION =
+  "Hey, I'm Zuxing Wu. Welcome to my personal blog where I share my thoughts on programming, technology, and life.";
